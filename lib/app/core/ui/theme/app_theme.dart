5406b2a3daec3e7eecf920dd179b3eb7efec6967
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-10-31 11:46:00 +0600

    create app necessary theme

---
+++ lib/app/core/ui/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:money_management_app/app/core/ui/theme/configs/dark_theme_config.dart';
+import 'package:money_management_app/app/core/ui/theme/configs/light_theme_config.dart';
+
+class AppTheme {
+  // Light theme configuration
+  static final ThemeData lightTheme = LightThemeConfig.themeConfig;
+
+  // Dark theme configuration
+  static final ThemeData darkTheme = DarkThemeConfig.themeConfig;
+}
