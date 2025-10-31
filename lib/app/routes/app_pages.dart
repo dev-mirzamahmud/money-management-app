9b98c94c126e8d7805e8ad932bd6551a017fd03d
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-10-31 23:23:42 +0600

    create pages and initial page

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+import 'package:money_management_app/app/features/auth/forgot_password/forgot_password_view.dart';
+import 'package:money_management_app/app/features/auth/otp/otp_view.dart';
+import 'package:money_management_app/app/features/auth/reset_password/reset_password_view.dart';
+import 'package:money_management_app/app/features/auth/sign_in/sign_in_view.dart';
+import 'package:money_management_app/app/features/auth/sign_up/sign_up_view.dart';
+import 'package:money_management_app/app/features/intro/onboard/onboard_view.dart';
+import 'package:money_management_app/app/features/intro/splash/splash_view.dart';
+import 'package:money_management_app/app/routes/routes.dart';
+
+class AppPages {
+  static const INITIAL = Routes.splash;
+
+  static final pages = [
+    // Initial Page
+    GetPage(name: Routes.splash, page: () => const SplashView()),
+
+    // OnBoarding Page
+    GetPage(name: Routes.onBoarding, page: () => const OnboardView()),
+
+    // Auth Pages
+    GetPage(name: Routes.signIn, page: () => const SignInView()),
+    GetPage(name: Routes.signUp, page: () => const SignUpView()),
+    GetPage(
+      name: Routes.forgotPassword,
+      page: () => const ForgotPasswordView(),
+    ),
+    GetPage(name: Routes.otpVerification, page: () => const OtpView()),
+    GetPage(name: Routes.resetPassword, page: () => const ResetPasswordView()),
+  ];
+}
