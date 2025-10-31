ab8f47106c61931d9d79af5b56804b18404ec249
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-10-31 23:23:16 +0600

    add intro and auth routes

---
+++ lib/app/routes/routes.dart
@@ -0,0 +1,14 @@
+class Routes {
+  // Initial Route
+  static const String splash = '/splash';
+
+  // OnBoarding Route
+  static const String onBoarding = '/onBoarding';
+
+  // Auth Routes
+  static const String signIn = '/signIn';
+  static const String signUp = '/signUp';
+  static const String forgotPassword = '/forgotPassword';
+  static const String otpVerification = '/otpVerification';
+  static const String resetPassword = '/resetPassword';
+}
