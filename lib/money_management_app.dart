1de1948068df4614fe1425bc9ae14db1953dc313
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-10-31 11:44:48 +0600

    add material app

--- lib/money_management_app.dart
+++ lib/money_management_app.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:money_management_app/app/core/constants/app_constants.dart';
+import 'package:money_management_app/app/core/ui/theme/app_theme.dart';
 
 class MoneyManagementApp extends StatelessWidget {
   const MoneyManagementApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: AppConstants.appName,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
+      defaultTransition: Transition.noTransition,
+      getPages: [],
+    );
   }
 }
