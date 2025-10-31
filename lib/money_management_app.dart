bfe1912334cc08aa9e828b4a237248f6e1ddfa69
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-11-01 01:14:56 +0600

    add initial bindings

--- lib/money_management_app.dart
+++ lib/money_management_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:money_management_app/app/core/constants/app_constants.dart';
 import 'package:money_management_app/app/core/ui/theme/app_theme.dart';
+import 'package:money_management_app/app/features/intro/splash/bindings/splash_binding.dart';
 import 'package:money_management_app/app/routes/app_pages.dart';
 
 class MoneyManagementApp extends StatelessWidget {
@@ -17,6 +18,7 @@ class MoneyManagementApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       defaultTransition: Transition.noTransition,
       initialRoute: AppPages.INITIAL,
+      initialBinding: SplashBinding(),
       getPages: AppPages.pages,
     );
   }
