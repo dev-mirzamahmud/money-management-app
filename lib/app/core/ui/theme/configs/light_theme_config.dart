1b07406d0ca6ec99fece9955cd662163a76f84f9
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-10-31 11:46:35 +0600

    create config for light theme

---
+++ lib/app/core/ui/theme/configs/light_theme_config.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:money_management_app/app/core/ui/colors/app_colors.dart';
+import 'package:money_management_app/app/core/ui/theme/widgets/t_appbar.dart';
+import 'package:money_management_app/app/core/ui/theme/widgets/t_color_scheme.dart';
+
+class LightThemeConfig {
+  static final ThemeData themeConfig = ThemeData(
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppColors.LIGHT_BG_COLOR,
+    colorScheme: TColorScheme.lightColorScheme,
+    appBarTheme: TAppbar.lightAppBarTheme,
+  );
+}
