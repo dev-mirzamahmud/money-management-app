1a75188b1005028f25fede7bad06ac67fbe9e9f9
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-11-01 01:16:15 +0600

    feat: (App Dimensions) add text font sizes

---
+++ lib/app/core/ui/dimensions/app_dimensions.dart
@@ -0,0 +1,22 @@
+class AppDimensions {
+  // Text -> Font Sizes
+  static const double bodySmall = 8;
+  static const double bodyMedium = 10;
+  static const double bodyLarge = 12;
+
+  static const double labelSmall = 12;
+  static const double labelMedium = 14;
+  static const double labelLarge = 16;
+
+  static const double titleSmall = 16;
+  static const double titleMedium = 18;
+  static const double titlelLarge = 20;
+
+  static const double headlineSmall = 22;
+  static const double headlineMedium = 24;
+  static const double headlinelLarge = 32;
+
+  static const double displaySmall = 44;
+  static const double displayMedium = 52;
+  static const double displaylLarge = 64;
+}
