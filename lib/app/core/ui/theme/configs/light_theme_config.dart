faf8e4cc1eb4edb930ff884d6b19cc45aa7dff44
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-11-01 01:17:01 +0600

    feat: (Light Theme Config) add primary text theme for light

--- lib/app/core/ui/theme/configs/light_theme_config.dart
+++ lib/app/core/ui/theme/configs/light_theme_config.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:money_management_app/app/core/ui/colors/app_colors.dart';
 import 'package:money_management_app/app/core/ui/theme/widgets/t_appbar.dart';
 import 'package:money_management_app/app/core/ui/theme/widgets/t_color_scheme.dart';
+import 'package:money_management_app/app/core/ui/theme/widgets/t_primary_text.dart';
 
 class LightThemeConfig {
   static final ThemeData themeConfig = ThemeData(
@@ -9,5 +10,6 @@ class LightThemeConfig {
     scaffoldBackgroundColor: AppColors.LIGHT_BG_COLOR,
     colorScheme: TColorScheme.lightColorScheme,
     appBarTheme: TAppbar.lightAppBarTheme,
+    primaryTextTheme: TPrimaryText.lightPrimaryTextTheme,
   );
 }
