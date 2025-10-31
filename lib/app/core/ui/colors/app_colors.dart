20036dfb2310fe45a76a915aba59a163206832b6
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-10-31 11:45:47 +0600

    add app necessary colors

---
+++ lib/app/core/ui/colors/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:money_management_app/app/core/helper/extensions/color_extensions.dart';
+
+class AppColors {
+  // App Colors - Light Theme
+  static Color LIGHT_PRIMARY_COLOR = 0xff1E3A8A.toColor;
+  static Color LIGHT_SECONDARY_COLOR = 0xff3B82F6.toColor;
+  static Color LIGHT_BG_COLOR = 0xffF9FAFB.toColor;
+  static Color LIGHT_SURFACE_COLOR = 0xffFFFFFF.toColor;
+  static Color LIGHT_TEXT_PRIMARY_COLOR = 0xff111827.toColor;
+  static Color LIGHT_TEXT_SECONDARY_COLOR = 0xff6B7280.toColor;
+  static Color LIGHT_ERROR_COLOR = 0xffEF4444.toColor;
+  static Color LIGHT_WARNING_COLOR = 0xffF59E0B.toColor;
+  static Color LIGHT_SUCCESS_COLOR = 0xff10B981.toColor;
+
+  // App Colors - Dark Theme
+  static Color DARK_PRIMARY_COLOR = 0xff60A5FA.toColor;
+  static Color DARK_SECONDARY_COLOR = 0xff22D3EE.toColor;
+  static Color DARK_BG_COLOR = 0xff1F2937.toColor;
+  static Color DARK_SURFACE_COLOR = 0xff374151.toColor;
+  static Color DARK_TEXT_PRIMARY_COLOR = 0xffF9FAFB.toColor;
+  static Color DARK_TEXT_SECONDARY_COLOR = 0xffD1D5DB.toColor;
+  static Color DARK_ERROR_COLOR = 0xffF87171.toColor;
+  static Color DARK_WARNING_COLOR = 0xffFBBF24.toColor;
+  static Color DARK_SUCCESS_COLOR = 0xff34D399.toColor;
+}
